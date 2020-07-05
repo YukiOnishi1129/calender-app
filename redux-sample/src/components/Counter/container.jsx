718e86fdc0afab574b33d402a3a5660b834eccf4
大西雄貴 <yuki.onishi1129@gmail.com>
2020-07-05 22:43:26 +0900

    redux-sample

--- redux-sample/src/components/Counter/container.jsx
+++ redux-sample/src/components/Counter/container.jsx
@@ -1,3 +1,18 @@
-import Counter from "./presentation";
+import { connect } from 'react-redux';
 
-export default Counter;
+import { increment, decrement } from '../../redux/count/actions';
+
+import Counter from './presentation';
+
+const mapStateProps = ({ count }) => ({ count });
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: (count) => {
+    dispatch(increment(count));
+  },
+  decrement: (count) => {
+    dispatch(decrement(count));
+  },
+});
+
+export default connect(mapStateProps, mapDispatchToProps)(Counter);
